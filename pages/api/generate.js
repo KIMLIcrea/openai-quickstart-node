2ced743c24380ce1bac66007be04dbf498e740f7
SineFine <benoitbarbagli@icloud.com>
2022-12-21 22:30:29 +0100

    personalistion

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,28 +1,30 @@
-import { Configuration, OpenAIApi } from "openai";
+// import { Configuration, OpenAIApi } from "openai";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
 
-export default async function (req, res) {
-  const completion = await openai.createCompletion({
-    model: "text-davinci-002",
-    prompt: generatePrompt(req.body.animal),
-    temperature: 0.6,
-  });
-  res.status(200).json({ result: completion.data.choices[0].text });
-}
+// export default async function (req, res) {
+//   const completion = await openai.createCompletion({
+//     model: "text-davinci-002",
+//     prompt: generatePrompt(req.body.animal),
+//     temperature: 0.6,
+//   });
+//   res.status(200).json({ result: completion.data.choices[0].text });
+// }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * max);
+// }
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
-}
+// function generatePrompt(animal) {
+//   const typeœuvres = ['sculpture', 'photographie', 'peinture', 'performance', 'instalation']
+//   const description = ['et décrit moi ses aspects politiques et sociaux', '', '', '']
+//   const langue = "(réponds moi en français)"
+//   const capitalizedAnimal =
+//     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+
+//   return `Cree .... ${typeœuvres[getRandomInt(typeœuvres.length)]} ....... ${description[getRandomInt(description.length)]} ..... ${langue[getRandomInt(langue.length)]} ...... ${capitalizedAnimal}`;
+
+// }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,22 @@
 import Head from "next/head";
 import { useState } from "react";
 import styles from "./index.module.css";
+import { Configuration, OpenAIApi } from "openai";
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function generatePrompt(animal) {
+  const typeœuvres = ['sculpture', 'photographie', 'peinture', 'performance', 'instalation']
+  const description = ['décrit moi ses aspects politiques et sociaux', '', '', '']
+  const langue = "(réponds moi en français)"
+  const capitalizedAnimal =
+    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+
+  return `Crée .... ${typeœuvres[getRandomInt(typeœuvres.length)]} et  ${description[getRandomInt(description.length)]} ..... ${langue[getRandomInt(langue.length)]} ...... ${capitalizedAnimal}`;
+
+}
 
 export default function Home() {
   const [animalInput, setAnimalInput] = useState("");
@@ -8,15 +24,25 @@ export default function Home() {
 
   async function onSubmit(event) {
     event.preventDefault();
-    const response = await fetch("/api/generate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ animal: animalInput }),
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
     });
-    const data = await response.json();
-    setResult(data.result);
+    const openai = new OpenAIApi(configuration);
+    
+    const completion = await openai.createCompletion({
+      model: "text-davinci-002",
+      prompt: generatePrompt(req.body.animal),
+      temperature: 0.6,
+    });
+    // const response = await fetch("/api/generate", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({ animal: animalInput }),
+    // });
+    // const data = await response.json();
+    setResult(completion.data.choices[0].text);
     setAnimalInput("");
   }
 
